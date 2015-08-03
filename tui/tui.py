d129073146794b57ebac5d52d6cd3d7828ba25b9
Alex Danoff <aadanoff@gmail.com>
2015-08-03 17:39:54 -0400

    Finish first pass of command mode
    Command mode allows the user to enter commands in a vim-like fashion.
    The only currently supported command is quit, however more will be
    added as the project progresses.

--- tui/tui.py
+++ tui/tui.py
@@ -8,22 +8,22 @@ This file implements the TUI functionality of the program.
 """
 
 import curses
-from ..itunes import itunes
 
-"""The top line of the usuable screen."""
-TOP = 2
+from enum import Enum
 
-"""The bottom line of the usable screen."""
-BOTTOM = curses.LINES - 2
+from itunes import itunes
 
-"""The leftmost column of the usable screen."""
-LEFT = 0
+"""Status codes returned by `command_mode` to indicate an action."""
+STATUS_CODES = Enum("StatusCodes", 'EXIT ERROR SEARCH')
 
-"""The rightmost column of the usable screen."""
-RIGHT = curses.COLS - 1
+"""Mapping of commands to actions."""
+COMMAND_MAP = {
+        "q": STATUS_CODES.EXIT,
+        "quit": STATUS_CODES.EXIT
+}
 
-"""The line allocated for entering commands."""
-COMMAND = curses.LINES - 1
+"""Constant for error color pair."""
+ERROR_PAIR = 1
 
 def main(stdscr):
     """
@@ -37,7 +37,69 @@ def main(stdscr):
         A `WindowObject` representing the main screen as passed by
         `curses.wrapper`.
     """
-    pass
+
+    # positioning variables
+    TOP_LINE = 2
+    BOTTOM_LINE = curses.LINES - 2
+    LEFT = 0
+    RIGHT = curses.COLS - 1
+    COMMAND_LINE = curses.LINES - 1
+
+    curses.init_pair(ERROR_PAIR, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    stdscr.addstr(0, 0, "Hello, world!")
+    stdscr.refresh()
+
+    command_win = curses.newwin(1, curses.COLS, COMMAND_LINE, LEFT)
+
+    command = None
+
+    # continue until quit command is given
+    while command != STATUS_CODES.EXIT:
+
+        key = stdscr.getkey()
+
+        # user wants to enter a command
+        if key == ":":
+            command = command_mode(command_win)
+            cursor_pos = stdscr.getyx()
+
+            # tell user they have entered an invalid command
+            if command == STATUS_CODES.ERROR:
+                stdscr.addstr(COMMAND_LINE, 0, "Invalid command entered", curses.color_pair(ERROR_PAIR))
+                stdscr.move(*cursor_pos)
+            elif command == STATUS_CODES.SEARCH:
+                pass
+
+def command_mode(window, line=0, col=0):
+    """
+    Receive commands from the user on a vim-like command line.
+
+    Parameters
+    ----------
+    window : curses.WindowObject
+        The window in which the command line should be placed.
+    line : int
+        The line in `window` on which to display the command line (default 0).
+    col : int
+        The column in `line` to display the command line (default 0). Note that
+        this is the column where the `:` character will be redisplayed, not
+        where the actual command will start.
+
+    Returns
+    -------
+    <Enum 'STATUS_CODES'>
+        The status code of the command entered by the user.
+    """
+
+    curses.echo()
+    window.clear()
+    window.addstr(line, col, ":")
+    command = window.getstr(line, col + 1)
+    command = command.decode("utf-8").lower()
+    curses.noecho()
+
+    return COMMAND_MAP.get(command, STATUS_CODES.ERROR)
 
 if __name__ == '__main__':
     curses.wrapper(main)
